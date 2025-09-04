88ecf3fb2232c880e82c0cae21ae75a71431df85
wintruz <wintruszenium@gmail.com>
2025-09-04 10:20:51 -0500

    Constructor y primer metodo

--- pcb.java
+++ pcb.java
@@ -1,16 +1,14 @@
+import java.util.Random;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Collections;
 
 public class pcb {
     //Atributos
-    int quantum; //Cantidad de instrucciones que puede realizar la CPU en un ms. Aleatorio entre 10-100 ms
     String id_PCB; // ID del proceso. Ej, P1
     int cant_Instruc; // Cantidad de instrucciones a realizar en el CPU
     String estado; // Estado del proceso
-    int cdc; // Cambio de contexto. Fijo en 1 ms
     String bloqueo; // Almacena "Si" o "No" de manera aleatoria
     int pos_Cola; // Posicion de llegada del proceso en la cola de ejecucion
     int tiempo_Cola; // Tiempo del proceso esperando en la cola para ejecutarse
@@ -18,8 +16,29 @@ public class pcb {
     int tiempo_Ejecutado; // Tiempo que ha pasado el proceso ejecutandose en el CPU
 
     //Constructor
-    
-    public pcb(){
+    public pcb(){ // Constructor vacio
         
+    }
+
+
+    public pcb(int i){
+        this.id_PCB = "P" + i;
+    }
+
+    // Metodos
+    public ArrayList<pcb> procesos() { //Creacion inicial de la lista de procesos de los n procesos dados por el usuario
+        ArrayList<pcb> listaProcesos = new ArrayList<>();
+        pcb temp = new pcb();
+
+        // Se obtienen la cantidad de procesos con Random
+        int cant;
+
+        // Creación de los n procesos en la lista
+        for(int i = 0;i < cant;i++){
+            temp = new pcb(i+1);
+            listaProcesos.add(temp);
+        }
+
+        return listaProcesos;
     }
 }
